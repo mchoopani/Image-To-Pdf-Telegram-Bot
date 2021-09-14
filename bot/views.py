487f8be1d4b80a84b4e3938317a7a2717520a817
Mahmood Choopani <choopani.m81@gmail.com>
2021-09-14 11:15:06 +0430

    delete useless data to optimzation in memory

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from json import loads
@@ -65,6 +67,18 @@ def convert_image_to_pdf(images: list, user_id):
     return f"{user_id}.pdf"
 
 
+def delete_files(downloaded_pictures, user_id):
+    for path in downloaded_pictures:
+        try:
+            os.remove(path)
+        except Exception:
+            pass
+    try:
+        os.remove(f"{user_id}.pdf")
+    except Exception:
+        pass
+
+
 @csrf_exempt
 def telegram_webhook(request, *args, **kwargs):
     message = Message(translate_request(request))
@@ -74,19 +88,25 @@ def telegram_webhook(request, *args, **kwargs):
         return JsonResponse({"ok": "POST request processed"})
     else:
         if message.text == "/start":
-            send_message(message.sender.id,"Hello welcome to your bot.\n"
-                                           "to start a project send: create\n"
-                                           "to end last active project send: export\n"
-                                           "after sending 'create' before history of projects will clear, "
-                                           "and start new project.\n"
-                                           "after send `export` all of pictures that send after last 'create'"
-                                           " and before 'export' will packed and converted to PDF.")
+            send_message(message.sender.id, "Hello welcome to your bot.\n"
+                                            "to start a project send: create\n"
+                                            "to end last active project send: export\n"
+                                            "after sending 'create' before history of projects will clear, "
+                                            "and start new project.\n"
+                                            "after send `export` all of pictures that send after last 'create'"
+                                            " and before 'export' will packed and converted to PDF.\n"
+                                            "shortcut: you can send your photos, at last send: !\n"
+                                            "this shortcut (!) collects before pictures and finally"
+                                            "clears the list of photos and waits for new project.")
             return JsonResponse({"ok": "POST request processed"})
         if message.text == "create":
+            send_message(message.sender.id, "new project created.")
             # TODO: delete from real database
             open(f"{message.sender.id}.txt", "w").close()
             return JsonResponse({"ok": "POST request processed"})
-        if message.text == "export":
+        if message.text == "export" or message.text == "!":
+            send_message(message.sender.id, "bot will export pdf of this project soon.\n"
+                                            "please wait...")
             downloaded_pictures = []
             try:
                 photos = select_photo_from_database(message.sender.id)
@@ -95,6 +115,9 @@ def telegram_webhook(request, *args, **kwargs):
                         .append(str(download_file(get_file(photos[i], message.sender.id), message.sender.id, i)))
                 convert_image_to_pdf(downloaded_pictures, message.sender.id)
                 send_document(message.sender.id, f"{message.sender.id}.pdf", None)
+                # TODO: delete from real database
+                open(f"{message.sender.id}.txt", "w").close()
+                delete_files(downloaded_pictures, message.sender.id)
             except Exception:
                 pass
             return JsonResponse({"ok": "POST request processed"})
